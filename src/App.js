959a377ea53e5d440e1aacfbcafda8ce2b1ceca7
AbuKausar007 <a.25kausar@gmail.com>
2022-04-03 16:46:07 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from "react";
 
 const App = () => {
   return (
     <div>
       <h1>Crypto Cafe Starter Template</h1>
+      <h3>hello</h3>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
